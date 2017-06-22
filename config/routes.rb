d0d8659774841113898b7099feb84a02f757fd7d
Aldo Sáez Jofré <aldo.saez.jofre@gmail.com>
2017-06-21 20:10:20 -0400

    [ADD] -> Tercer commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/batman'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get 'pages/batman'
+    get 'pages/superman'
+
+
 end

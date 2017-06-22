1b74ab2eaa9b9161f17ee41e6a38e2d844ab09b9
Aldo Sáez Jofré <aldo.saez.jofre@gmail.com>
2017-06-21 20:55:47 -0400

    [ADD] -> Quinto commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
-  def batman
-  end
+    def batman
+
+    end
+
+    def save_votos_batman
+        VotosBatman.create(name: params[:name], email: params[:email])
+        redirect_to root_path, notice: "El voto fue guardado correctamente"
+    end
+
+    def save_votos_superman
+        VotosSuperman.create(name: params[:name], email: params[:email])
+        redirect_to root_path, notice: "El voto fue guardado correctamente"
+    end
+
+    def batman_vs_superman
+        @votos_batman = VotosSuperman.all
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
-    root 'pages#batman-vs-superman'
+    root 'pages#batman_vs_superman'
     get  'pages/batman'
     get  'pages/superman'
+
+    post 'pages/save_votos_batman'
+    post 'pages/save_votos_superman'
 
 
 end
